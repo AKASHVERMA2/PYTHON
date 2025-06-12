f613625763d7ee07dee00f8b80e2041f977be473
AKASHVERMA2 <hiamakash7571@gmail.com>
2025-06-12 14:24:11 +0530

    add a new paragraph

---
+++ Chapter 1.py
@@ -0,0 +1,7 @@
+print ("hello world")
+# Install a module with the help of pip name pyjokes
+# pyjokes module make many type of joke without write any code
+
+import pyjokes
+joke  = pyjokes.get_joke()
+print(joke)
