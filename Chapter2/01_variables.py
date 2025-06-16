de07cb84916b6c5356b672bd31b82072243be9f5
AKASHVERMA2 <hiamakash7571@gmail.com>
2025-06-16 15:37:25 +0530

    add new global variable conntent

--- Chapter2/01_variables.py
+++ Chapter2/01_variables.py
@@ -116,3 +116,92 @@ x = "Python"
 y = "is"
 z = "awesome"
 print(x, y, z)
+
+# You can also use the + operator to output multiple variables
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+# Notice the space character after "Python " and "is "
+# without them the result would be "Pythonisawesome".
+
+# For numbers, the + character works as a mathematical operator
+x = 5
+y = 10
+print(x + y)
+
+# In the print() function, when you try to combine a string and a number with the + operator,
+# Python will give you an error
+
+# x = 5
+# y = "John"
+# print(x + y)
+
+
+# The best way to output multiple variables in the print() function is to separate them with commas,
+# which even support different data types
+x = 5
+y = "John"
+print(x, y)
+
+# Python - Global Variables
+# Variables that are created outside of a function (as in all of the examples in the previous pages) 
+#  are known as global variables.
+# Global variables can be used by everyone, both inside of functions and outside.
+
+# Ex.-
+
+# Create a variable outside of a function, and use it inside the function
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+# If you create a variable with the same name inside a function, this variable will be local,
+# and can only be used inside the function. The global variable with the same name will remain as it was,
+# global and with the original value.
+
+# Ex.-
+# Create a variable inside a function, with the same name as the global variable
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+# The global Keyword
+# Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
+# To create a global variable inside a function, you can use the global keyword.
+
+# Ex.-
+
+# If you use the global keyword, the variable belongs to the global scope
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+# Also, use the global keyword if you want to change a global variable inside a function
+
+# Ex.-
+
+# To change the value of a global variable inside a function, refer to the variable by using the global keyword
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
